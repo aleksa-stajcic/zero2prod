058b05f847038098c673a1e78b7629c753d9bb9d
aleksa-stajcic <aleksa@byteout.com>
2023-05-29 22:43:14 +0200

    api: basic health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(clippy::implicit_return)]
+#![allow(clippy::needless_return)]
+
+use actix_web::{HttpRequest, Responder, HttpServer, App, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    return format!("Hello {}!", &name);
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    return HttpServer::new(|| {
+        return App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet));
+    })
+    .bind("127.0.0.1:6969")?
+    .run()
+    .await;
 }
